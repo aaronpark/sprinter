47290a7a613bc70045c04bfaa722c8c52fc1a67e
Aaron Park <apark@wantsa.com>
2012-05-31 16:31:20 -0700

    put the reload into the model

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,4 +1,40 @@
 class Sprint < ActiveRecord::Base
+  include ApplicationHelper
   has_many :cards
   attr_accessible :end_date, :name, :start_date, :xid
+
+  def reload
+    project = HTTParty.get("http://jira.wantsa.com:38881/rest/api/2/project/WP",{:basic_auth => auth})
+    
+    project['versions'].each do |sprint|
+      @sprint = Sprint.find_by_xid(sprint['id'])
+      if !@sprint
+        @sprint = Sprint.create({:xid => sprint['id'], :name => sprint['name']})
+      end
+      
+      sprint = HTTParty.get("http://jira.wantsa.com:38881/rest/api/2/search?jql=fixVersion=#{@sprint.xid}",{:basic_auth => auth})
+      sprint['issues'].each do |sprint_card|
+        @card = Card.find_by_key(sprint_card['key'])
+        if !@card
+          @card = Card.create({ :key => sprint_card['key'] })
+        end
+        
+        @card.summary = sprint_card['fields']['summary']
+        @card.sprint_id = @sprint.id
+        @card.description = sprint_card['fields']['description']
+        if sprint_card['fields']['assignee']
+          @card.assignee = sprint_card['fields']['assignee']['displayName']
+        else
+          @card.assignee = 'Unassigned'
+        end
+        @card.card_created = sprint_card['fields']['created']
+        @card.card_updated = sprint_card['fields']['updated']
+        @card.card_type = sprint_card['fields']['issuetype']['name']
+        @card.points = sprint_card['fields']['customfield_10013']
+        @card.status = sprint_card['fields']['status']['name']
+        @card.save! 
+      end
+    end
+  end
+  
 end
