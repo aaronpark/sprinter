128125105caaecf3efb6587f5706b7807a02b54a
Aaron Park <apark73@gmail.com>
2012-06-09 09:13:11 -0700

    adds version number and about page

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
+#APP_VERSION = '0.1.1'
+
 # Initialize the rails application
 Sprinter::Application.initialize!
