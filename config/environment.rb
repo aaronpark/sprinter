abf267973d6814afc3cbc19d992da6ee0fc1186b
Aaron Park <apark73@gmail.com>
2012-06-09 10:47:19 -0700

    changes git describe to git tag

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_VERSION = `git describe --always` unless defined? APP_VERSION
+APP_VERSION = `git tag` unless defined? APP_VERSION
 #APP_VERSION = '0.1.1'
 
 # Initialize the rails application
